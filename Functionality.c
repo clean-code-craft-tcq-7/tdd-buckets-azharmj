d4857577492a3f65b9d7e6c61bcfe3db4ca7209f
azharmj <46325751+azharmj@users.noreply.github.com>
2023-01-30 18:47:49 +0530

    Update Functionality.c

--- Functionality.c
+++ Functionality.c
@@ -5,25 +5,31 @@
 void CreateInformat(int lowLimit,int highLimit,int counter, char* buff)
 {
   
-char Data[100];
-sprintf(Data,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
-strncpy(buff,Data,strlen(Data));
+  char Data[100];
+  sprintf(Data,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
+  strncpy(buff,Data,strlen(Data));
+  
 }
 void doSort(int *A,int len)
-{int tmp,i,j;
- for(i=0;i<len;i++){
-   for(j=0;j<(len-i-1);j++){
-     if(A[j]>A[j+1]){
-tmp = A[j];
-A[j] = A[j+1];
-A[j+1] = tmp;
-}}}}
+{
+  int tmp,i,j;
+  for(i=0;i<len;i++){
+    for(j=0;j<(len-i-1);j++){
+      if(A[j]>A[j+1]){
+        tmp = A[j];
+        A[j] = A[j+1];
+        A[j+1] = tmp;
+      }
+    }
+  }
+}
 int CountValue(int *A,int len, int low,int high)
 {
-int i,cnt = 1;
-for(i=0;i<=len;i++){
-  if(A[i]>=low && A[i]<=high){
+  int i,cnt = 1;
+  for(i=0;i<=len;i++){
+    if(A[i]>=low && A[i]<=high){
       cnt++;
-}}
+    }
+  }
   return cnt;
 }
