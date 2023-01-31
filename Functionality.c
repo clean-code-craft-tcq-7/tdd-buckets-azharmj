c4d31b2b35fba54bde3cdaa3f3e59776af59ad67
azharmj <46325751+azharmj@users.noreply.github.com>
2023-01-31 12:08:21 +0530

    Update Functionality.c

--- Functionality.c
+++ Functionality.c
@@ -2,33 +2,50 @@
 #include <string.h>
 #include "Functionality.h"
 
+#define checkrange(a, low, high)  (a>=low && a<=high)
+
+static void swapelement(int a, int b);
+
 void CreateInformat(int lowLimit,int highLimit,int counter, char* buff)
 {
-  
-  char Data[100];
   sprintf(buff,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
-  printf("%s",buff);
 }
+
+
 void doSort(int *A,int len)
 {
   int tmp,i,j;
-  for(i=0;i<len;i++){
-    for(j=0;j<(len-i-1);j++){
-      if(A[j]>A[j+1]){
-        tmp = A[j];
-        A[j] = A[j+1];
-        A[j+1] = tmp;
-      }
-    }
-  }
+ for(i=0;i<len;i++)
+ {
+   for(j=0;j<(len-i-1);j++)
+   { 
+    swapelement(A[j],A[j+1]);
+   }
+ }
 }
+
+
 int CountValue(int *A,int len, int low,int high)
 {
-  int i,cnt = 1;
-  for(i=0;i<=len;i++){
-    if(A[i]>=low && A[i]<=high){
+int i,cnt = 0;
+for(i=0;i<=len;i++)
+{
+  if(checkrange(A[i], low, high))
+  {
       cnt++;
-    }
   }
+}
   return cnt;
 }
+
+
+ static void swapelement(int a, int b)
+ {
+   int tmp;
+   if(a>b)
+   {
+      tmp = a;
+      a = b;
+      b = tmp;
+   }
+ }
