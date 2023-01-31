0e2850c2d372b6c8a7431bab9cf9ce125f7d47be
azharmj <46325751+azharmj@users.noreply.github.com>
2023-01-31 12:11:33 +0530

    Update Testcase.c

--- Testcase.c
+++ Testcase.c
@@ -4,15 +4,25 @@
 #include <string.h>
 #include "Functionality.h"
 
-
 int main(void)
 {
-  char output[100];
-  memset(output, 0, 100);
  
- CreateInformat(4,5,2, output);
+ int current_samples[7]={3, 3, 5, 4, 10, 11, 12};
+ int storecount;
+ char output[100];
+ memset(output, 0, 100);
  
- assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+
+doSort(current_samples,7);
+storecount = CountValue(current_samples,7,3,5);
+CreateInformat(3,5,storecount,output);
+ 
+ printf("%s\n", output);
+ 
+ storecount = CountValue(current_samples,7,10,12);
+ CreateInformat(10,12,storecount,output);
+  printf("%s", output);
+// assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
   
 return 0;
   
